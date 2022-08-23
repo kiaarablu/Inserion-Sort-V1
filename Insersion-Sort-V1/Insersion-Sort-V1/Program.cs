a77a4012cb5c36c4b03b9c967c7c390399e766b4
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-23 10:31:35 +0430

    Add files via upload

---
+++ Insersion-Sort-V1/Insersion-Sort-V1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insersion_Sort_V1
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+
+        {
+            int[] array = { 15,-2,20,16,30,30,50,-23,0 };
+            Sorting k = new Sorting();
+
+            k.Sort(array);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Insersion-Sort-V1/Insersion-Sort-V1/Sorting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insersion_Sort_V1
+{
+    internal class Sorting
+    {
+
+        int j = 1;
+        int n = 0;
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int k = j;
+                while (k > n)
+                {
+                    if (array[k] < array[k - 1])
+                    {
+                        int temp = array[k];
+                        array[k] = array[k - 1];
+                        array[k - 1] = temp;
+                        
+                    }
+                    k--;
+
+                }
+                j++;
+                k = j;
+            }
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+
+    }
+}
